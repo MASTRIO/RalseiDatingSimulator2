f707daa0e46973739d28c12fd3aa87ee34468be2
MASTRIO <mastrio5772@gmail.com>
2022-02-23 17:59:56 +1000

    amogus

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,18 +13,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.ML;
 
 namespace RalseiDatingSimulator2
 {
     public partial class MainWindow : Window
     {
-        //RalseiConversationAI.ModelInput sampleData;
-        //RalseiConversationAI.ModelOutput result;
+        string input = "";
+        string output = "";
 
         public MainWindow()
         {
             InitializeComponent();
             ResizeMode = ResizeMode.NoResize;
+
+            DialogTitle.Visibility = Visibility.Hidden;
+            DialogContent.Visibility = Visibility.Hidden;
+            DialogClose.Visibility = Visibility.Hidden;
+            DialogInput.Visibility = Visibility.Hidden;
+            DialogBackground.Visibility = Visibility.Hidden;
         }
 
         private void ProcessInput(object sender, RoutedEventArgs e)
@@ -35,12 +43,86 @@ namespace RalseiDatingSimulator2
 
             var result = RalseiConversationAI.Predict(sampleData);
 
+            input = EnterText.Text;
+            output = result.PredictedLabel;
+
             OutputText.Content = result.PredictedLabel;
         }
 
         private void EnterText_Focused(object sender, RoutedEventArgs e)
         {
             OutputText.Content = "Yes?";
+        }
+
+        private void GoodAnswer_Clicked(object sender, RoutedEventArgs e)
+        {
+            OutputText.Content = "Yay! I'm trying to improve all the time!";
+
+            AddData();
+        }
+
+        private void BadAnswer_Clicked(object sender, RoutedEventArgs e)
+        {
+            OutputText.Content = "I'll try to do better next time :)";
+
+            OpenDialog("Bad answer :(", "How can I improve my answer?\n\nInput: \"" + input + "\"");
+        }
+
+        private void DialogClose_Clicked(object sender, RoutedEventArgs e)
+        {
+            DialogTitle.Visibility = Visibility.Hidden;
+            DialogContent.Visibility = Visibility.Hidden;
+            DialogClose.Visibility = Visibility.Hidden;
+            DialogInput.Visibility = Visibility.Hidden;
+            DialogBackground.Visibility = Visibility.Hidden;
+
+            output = DialogInput.Text;
+            AddData();
+        }
+
+        private void RebuildPipeline_Clicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This doesn't work yet (if it ever will)", "no");
+
+            /*
+            MLContext mlContext = new MLContext();
+            mlContext.Data.LoadFromTextFile("NewMachineLearningData.txt");
+
+            RalseiConversationAI.BuildPipeline(mlContext);
+            */
+        }
+
+        void OpenDialog(string title, string content)
+        {
+            DialogTitle.Content = title;
+            DialogContent.Content = content;
+
+            DialogTitle.Visibility = Visibility.Visible;
+            DialogContent.Visibility = Visibility.Visible;
+            DialogClose.Visibility = Visibility.Visible;
+            DialogInput.Visibility = Visibility.Visible;
+            DialogBackground.Visibility = Visibility.Visible;
+        }
+
+        void AddData()
+        {
+            string path = @"NewMachineLearningData.csv";
+
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(input + "," + output);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(input + "," + output);
+                }
+            }
         }
     }
 }
